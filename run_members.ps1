44d4627f8dcda117debd541c4c664b2af6484717
Miguel Bibi <miguel.c.bibi@gmail.com>
2025-03-10 16:09:52 +0000

    feat: Add functionality to open a terminal for the Leader in run_members.ps1

--- run_members.ps1
+++ run_members.ps1
@@ -1,3 +1,6 @@
+# Open a terminal for the Leader
+Start-Process powershell -ArgumentList "-NoExit", "-Command", "& {mvn exec:java '-Dexec.mainClass=com.example.Leader'}"
+
 # Open 4 terminals for members
 for ($i = 1; $i -le 4; $i++) {
     Start-Process powershell -ArgumentList "-NoExit", "-Command", "& {mvn exec:java '-Dexec.mainClass=com.example.Member' '-Dexec.args=member$i'}"
